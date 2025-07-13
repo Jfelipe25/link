aa0b2fc5280cd16be4a61f9ef073ff44292d19e8
Jfelipe25 <felipergz12@gmail.com>
2025-07-13 20:59:58 +0000

    fix: convierte campos string a JSON para columnas jsonb

--- utils/saveProfile.js
+++ utils/saveProfile.js
@@ -1,24 +1,24 @@
 export async function saveProfile(data) {
-  console.log("Guardando perfil en Supabase:", data) // <--- este log es clave
+  console.log("Guardando perfil en Supabase:", data)
 
   const { error } = await supabase
     .from('profiles')
     .insert([{
       name: data.n,
       description: data.d,
-      image: data.i,
-      facebook: data.f,
-      twitter: data.t,
-      instagram: data.ig,
-      github: data.gh,
-      telegram: data.tg,
-      linkedin: data.l,
-      email: data.e,
-      whatsapp: data.w,
-      youtube: data.y,
-      tiktok: data.tk,
-      onlyfans: data.of,
-      links: data.ls
+      image: JSON.stringify(data.i),
+      facebook: JSON.stringify(data.f),
+      twitter: JSON.stringify(data.t),
+      instagram: JSON.stringify(data.ig),
+      github: JSON.stringify(data.gh),
+      telegram: JSON.stringify(data.tg),
+      linkedin: JSON.stringify(data.l),
+      email: JSON.stringify(data.e),
+      whatsapp: JSON.stringify(data.w),
+      youtube: JSON.stringify(data.y),
+      tk: JSON.stringify(data.tk),
+      onlyfans: JSON.stringify(data.of),
+      links: data.ls // ya es array de objetos, así está bien como jsonb
     }])
 
   if (error) {
