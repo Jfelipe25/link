f3702cee40d0c039a90a43a963b20cc2b0d70dda
Jfelipe25 <felipergz12@gmail.com>
2025-07-13 21:05:01 +0000

    fix: estructura correcta para campos jsonb

--- utils/saveProfile.js
+++ utils/saveProfile.js
@@ -1,3 +1,5 @@
+import { supabase } from './supabase'
+
 export async function saveProfile(data) {
   console.log("Guardando perfil en Supabase:", data)
 
@@ -6,19 +8,19 @@ export async function saveProfile(data) {
     .insert([{
       name: data.n,
       description: data.d,
-      image: JSON.stringify(data.i),
-      facebook: JSON.stringify(data.f),
-      twitter: JSON.stringify(data.t),
-      instagram: JSON.stringify(data.ig),
-      github: JSON.stringify(data.gh),
-      telegram: JSON.stringify(data.tg),
-      linkedin: JSON.stringify(data.l),
-      email: JSON.stringify(data.e),
-      whatsapp: JSON.stringify(data.w),
-      youtube: JSON.stringify(data.y),
-      tk: JSON.stringify(data.tk),
-      onlyfans: JSON.stringify(data.of),
-      links: data.ls // ya es array de objetos, así está bien como jsonb
+      image: { url: data.i },
+      facebook: { url: data.f },
+      twitter: { url: data.t },
+      instagram: { url: data.ig },
+      github: { url: data.gh },
+      telegram: { url: data.tg },
+      linkedin: { url: data.l },
+      email: { address: data.e },
+      whatsapp: { number: data.w },
+      youtube: { url: data.y },
+      tk: { url: data.tk },
+      onlyfans: { url: data.of },
+      links: data.ls // array de objetos, compatible con jsonb
     }])
 
   if (error) {
